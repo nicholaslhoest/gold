10daab40a48accc272ff4e717813794c05fc4662
Nicholas B. Lhoest (Captain) <nicholas.lhoest@lhoeststudios.com>
2025-02-20 23:59:38 +0100

    ACT-48: (feat) Big Update

---
+++ src/content/content.config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const papers = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/content/papers" }),
+    schema: z.object({
+        title: z.string(),
+        author: z.string(),
+        date: z.string(),
+        tags: z.array(z.string()).optional(),
+    }),
+});
+
+export const collections = { papers };
